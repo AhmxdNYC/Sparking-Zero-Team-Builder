05e7571a79bd21009696dcb9aef07cc1da8bc2d6
ahmad <ahmadhamza052005@gmail.com>
2024-10-18 00:17:31 -0400

    changed teamUrl generator

--- docs/src/components/LoadUrl.tsx
+++ docs/src/components/LoadUrl.tsx
@@ -12,7 +12,7 @@ interface UseLoadTeamFromURLProps {
 	allCharacters: Character[]; // The full character list for reference
 }
 
-// Hook to load the team from the URL
+// Hook to load the team from the URL (supports hash-based URLs)
 const useLoadTeamFromURL = ({
 	setCurrentTeam,
 	allCharacters,
@@ -20,24 +20,24 @@ const useLoadTeamFromURL = ({
 	const location = useLocation();
 
 	useEffect(() => {
-		console.log('Current location hash:', location.hash);
-		// Parse the query string after the hash in the URL
-		const params = new URLSearchParams(location.hash.split('?')[1]);
-		const teamParam = params.get('team');
+		// For HashRouter, we need to check the hash portion of the URL
+		const hash = location.hash || window.location.hash;
+		console.log('Current location hash:', hash);
 
+		// Parse the query string from the hash portion of the URL
+		const params = new URLSearchParams(hash.replace('#/', ''));
+		const teamParam = params.get('team');
 		console.log('Parsed team parameter:', teamParam);
+
 		if (teamParam) {
 			// Deserialize the character names from the URL
 			const characterNames = decodeURIComponent(teamParam).split(',');
-
 			console.log('Character names from URL:', characterNames);
 
 			// Filter the characters based on the names in the URL
 			const selectedTeam = allCharacters.filter((character) =>
 				characterNames.includes(character.name)
 			);
-
-			// Log the selected team based on character names
 			console.log('Selected team based on URL:', selectedTeam);
 
 			// Set the current team based on the selected characters
