9354db1471b4d53b05538df43fa181952d5d322f
ahmad <ahmadhamza052005@gmail.com>
2024-10-29 14:53:45 -0400

    attemp copy after build

--- docs/src/components/SearchBar.tsx
+++ docs/src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { SearchBarProps } from './types';
-import DropDownList from './DropDown';
+import DropDownList from './TempDropDownList';
 // idkk
 const SearchBar: React.FC<SearchBarProps> = ({
 	data,
